bf45347a39a694834f3b2577236d44371b396048
phucanhle <lephucanh2601@gmail.com>
2025-02-27 01:04:25 +0700

    FT: Update route search on '/api/ingredient?search='

--- src/app/api/ingredients/route.ts
+++ src/app/api/ingredients/route.ts
@@ -1,12 +1,23 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { supabase } from "@/lib/supabase";
 
-export async function GET() {
-    const { data, error } = await supabase
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url);
+
+    const search = searchParams.get("search");
+    let query = supabase
         .from("foods")
         .select(
             `id, ingredient_name, ingredient_des, group_id, food_groups(name)`
         );
+
+    if (search) {
+        query = query.ilike(
+            "ingredient_name",
+            `%${search}%`
+        );
+    }
+    const { data, error } = await query;
 
     if (error) {
         return NextResponse.json(
